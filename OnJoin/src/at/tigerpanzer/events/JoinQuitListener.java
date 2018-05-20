d9719703aaad4860fa67e1e00edd6106049d7d77
Plajer <worthplay.pt@onet.pl>
2018-05-20 15:47:52 +0200

    Event priority normal is redundant

--- OnJoin/src/at/tigerpanzer/events/JoinQuitListener.java
+++ OnJoin/src/at/tigerpanzer/events/JoinQuitListener.java
@@ -16,7 +16,7 @@ import org.bukkit.event.player.PlayerQuitEvent;
 
 public class JoinQuitListener implements Listener {
 
-    @EventHandler(priority = EventPriority.NORMAL)
+    @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
         if(FileManager.getString("Join.JoinSoundOn").contains("true")) {
@@ -27,24 +27,20 @@ public class JoinQuitListener implements Listener {
         } else if(FileManager.getString("Join.JoinMessageOn").contains("false")) {
             e.setJoinMessage("");
         }
-        Bukkit.getScheduler().runTaskLater(Main.getInstance(), new Runnable() {
-            @Override
-            public void run() {
-
-                if(FileManager.getString("Title.TitleOnJoin").contains("true")) {
-                    TitleUtils.sendFullTitle(p, 25, 90, 0, FileManager.getString("Title.Title1").replaceAll("%player%", p.getDisplayName()), FileManager.getString("Title.SubTitle1").replaceAll("%player%", p.getDisplayName()));
-                    if(FileManager.getString("actionbar.actionbaronjoin").contains("true")) {
-                        ActionbarUtils.sendActionBar(p, Utils.color(FileManager.getString("actionbar.actionbar1").replaceAll("%player%", p.getDisplayName())));
-                    }
-                    if(FileManager.getString("WelcomeMessage.WelcomeMessageOn").contains("true")) {
-                        Player p = e.getPlayer();
-                        p.sendMessage(Utils.color(FileManager.getString("Prefix") + FileManager.getString("WelcomeMessage.WelcomeMessageHeader").replaceAll("%player%", p.getDisplayName())));
-                        p.sendMessage(Utils.color(FileManager.getString("Prefix") + FileManager.getString("WelcomeMessage.WelcomeMessageLine1").replaceAll("%player%", p.getDisplayName())));
-                        p.sendMessage(Utils.color(FileManager.getString("Prefix") + FileManager.getString("WelcomeMessage.WelcomeMessageLine2").replaceAll("%player%", p.getDisplayName())));
-                        p.sendMessage(Utils.color(FileManager.getString("Prefix") + FileManager.getString("WelcomeMessage.WelcomeMessageLine3").replaceAll("%player%", p.getDisplayName())));
-                        p.sendMessage(Utils.color(FileManager.getString("Prefix") + FileManager.getString("WelcomeMessage.WelcomeMessageLine4").replaceAll("%player%", p.getDisplayName())));
-                        p.sendMessage(Utils.color(FileManager.getString("Prefix") + FileManager.getString("WelcomeMessage.WelcomeMessageFooder").replaceAll("%player%", p.getDisplayName())));
-                    }
+        Bukkit.getScheduler().runTaskLater(Main.getInstance(), () -> {
+            if(FileManager.getString("Title.TitleOnJoin").contains("true")) {
+                TitleUtils.sendFullTitle(p, 25, 90, 0, FileManager.getString("Title.Title1").replaceAll("%player%", p.getDisplayName()), FileManager.getString("Title.SubTitle1").replaceAll("%player%", p.getDisplayName()));
+                if(FileManager.getString("actionbar.actionbaronjoin").contains("true")) {
+                    ActionbarUtils.sendActionBar(p, Utils.color(FileManager.getString("actionbar.actionbar1").replaceAll("%player%", p.getDisplayName())));
+                }
+                if(FileManager.getString("WelcomeMessage.WelcomeMessageOn").contains("true")) {
+                    Player p1 = e.getPlayer();
+                    p1.sendMessage(Utils.color(FileManager.getString("Prefix") + FileManager.getString("WelcomeMessage.WelcomeMessageHeader").replaceAll("%player%", p1.getDisplayName())));
+                    p1.sendMessage(Utils.color(FileManager.getString("Prefix") + FileManager.getString("WelcomeMessage.WelcomeMessageLine1").replaceAll("%player%", p1.getDisplayName())));
+                    p1.sendMessage(Utils.color(FileManager.getString("Prefix") + FileManager.getString("WelcomeMessage.WelcomeMessageLine2").replaceAll("%player%", p1.getDisplayName())));
+                    p1.sendMessage(Utils.color(FileManager.getString("Prefix") + FileManager.getString("WelcomeMessage.WelcomeMessageLine3").replaceAll("%player%", p1.getDisplayName())));
+                    p1.sendMessage(Utils.color(FileManager.getString("Prefix") + FileManager.getString("WelcomeMessage.WelcomeMessageLine4").replaceAll("%player%", p1.getDisplayName())));
+                    p1.sendMessage(Utils.color(FileManager.getString("Prefix") + FileManager.getString("WelcomeMessage.WelcomeMessageFooder").replaceAll("%player%", p1.getDisplayName())));
                 }
             }
         }, 2L);
@@ -58,11 +54,9 @@ public class JoinQuitListener implements Listener {
 
             }
         }, 65L);
-
-
     }
 
-    @EventHandler(priority = EventPriority.NORMAL)
+    @EventHandler
     public void onQuit(PlayerQuitEvent e) {
         Player p = e.getPlayer();
         if(FileManager.getString("Quit.QuitSoundOn").contains("true")) {
