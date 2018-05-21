29dc6de2aeb28f77657df21a458a01bc355c3e65
Tigerpanzer_02 <37453987+Tigerpanzer02@users.noreply.github.com>
2018-05-21 12:37:57 +0200

    Upload fail

--- OnJoin/src/at/tigerpanzer/events/JoinQuitListener.java
+++ OnJoin/src/at/tigerpanzer/events/JoinQuitListener.java
@@ -20,7 +20,6 @@ public class JoinQuitListener implements Listener {
         if(Main.getInstance().getConfig().getString("Join.ChatClearOn").contains("true")) {
             for (int i = 0; i < 200; i++) {
                 p.sendMessage(" ");
-
             }
         }
         if(Main.getInstance().getConfig().getString("Join.JoinSoundOn").contains("true")) {
