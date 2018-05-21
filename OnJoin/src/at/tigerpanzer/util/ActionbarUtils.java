460982aca32ed5255cd90b8ba3d3c01c6e8b243e
Plajer <worthplay.pt@onet.pl>
2018-05-21 17:29:32 +0200

    Sending spigot's action bar for 1.9+ servers

--- OnJoin/src/at/tigerpanzer/util/ActionbarUtils.java
+++ OnJoin/src/at/tigerpanzer/util/ActionbarUtils.java
@@ -1,5 +1,8 @@
 package at.tigerpanzer.util;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.Constructor;
@@ -8,17 +11,21 @@ import java.lang.reflect.InvocationTargetException;
 public class ActionbarUtils {
 
     public static void sendActionBar(Player player, String message) {
-        try {
-            Constructor<?> constructor = ReflectionUtils.getNMSClass("PacketPlayOutChat").getConstructor(ReflectionUtils.getNMSClass("IChatBaseComponent"), byte.class);
+        if(Bukkit.getServer().getVersion().contains("v1_7") || Bukkit.getServer().getVersion().contains("v1_8")) {
+            try {
+                Constructor<?> constructor = ReflectionUtils.getNMSClass("PacketPlayOutChat").getConstructor(ReflectionUtils.getNMSClass("IChatBaseComponent"), byte.class);
 
-            Object icbc = ReflectionUtils.getNMSClass("IChatBaseComponent").getDeclaredClasses()[0].getMethod("a", String.class).invoke(null, "{\"text\":\"" + message + "\"}");
-            Object packet = constructor.newInstance(icbc, (byte) 2);
-            Object entityPlayer = player.getClass().getMethod("getHandle").invoke(player);
-            Object playerConnection = entityPlayer.getClass().getField("playerConnection").get(entityPlayer);
+                Object icbc = ReflectionUtils.getNMSClass("IChatBaseComponent").getDeclaredClasses()[0].getMethod("a", String.class).invoke(null, "{\"text\":\"" + message + "\"}");
+                Object packet = constructor.newInstance(icbc, (byte) 2);
+                Object entityPlayer = player.getClass().getMethod("getHandle").invoke(player);
+                Object playerConnection = entityPlayer.getClass().getField("playerConnection").get(entityPlayer);
 
-            playerConnection.getClass().getMethod("sendPacket", ReflectionUtils.getNMSClass("Packet")).invoke(playerConnection, packet);
-        } catch(NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchFieldException | InstantiationException e) {
-            e.printStackTrace();
+                playerConnection.getClass().getMethod("sendPacket", ReflectionUtils.getNMSClass("Packet")).invoke(playerConnection, packet);
+            } catch(NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchFieldException | InstantiationException e) {
+                e.printStackTrace();
+            }
+        } else {
+            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new ComponentBuilder(message).create());
         }
     }
 
