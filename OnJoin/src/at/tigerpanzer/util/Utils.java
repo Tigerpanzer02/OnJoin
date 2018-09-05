353caaf3b1bf846d3b5089ebfcbce320ed88ca74
Plajer <worthplay.pt@onet.pl>
2018-09-05 18:28:51 +0200

    Using better StringUtils

--- OnJoin/src/at/tigerpanzer/util/Utils.java
+++ OnJoin/src/at/tigerpanzer/util/Utils.java
@@ -2,6 +2,7 @@ package at.tigerpanzer.util;
 
 import at.tigerpanzer.Main;
 import me.clip.placeholderapi.PlaceholderAPI;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -13,21 +14,22 @@ public class Utils {
         return ChatColor.translateAlternateColorCodes('&', msg);
     }
 
-    public static String setPlaceholders(final Player p, String s) {
+    public static String setPlaceholders(final Player p, String str) {
+        String formatted = str;
         if(Main.getInstance().placeholderAPI) {
-            s = PlaceholderAPI.setPlaceholders(p, s);
+            formatted = PlaceholderAPI.setPlaceholders(p, str);
         }
-        s = s.replace("%prefix%", Main.getInstance().getConfig().getString("Prefix"));
-        s = s.replace("%player%", p.getName());
-        s = s.replace("%onjoin-player-displayname%", p.getDisplayName());
-        s = s.replace("%onjoin-player-uuid%", p.getUniqueId().toString());
-        s = s.replace("%onjoin-player-gamemode%", p.getGameMode().name());
-        s = s.replace("%onjoin-world%", p.getWorld().getName());
-        s = s.replace("%onjoin-player-health%", String.valueOf(p.getHealth()));
-        s = s.replace("%onjoin-player-max-health%", String.valueOf(p.getMaxHealth()));
-        s = s.replace("%onjoin-max-players%", String.valueOf(Bukkit.getServer().getMaxPlayers()));
-        s = s.replace("%onjoin-online-players%", String.valueOf(Bukkit.getServer().getOnlinePlayers().size()));
-        s = ChatColor.translateAlternateColorCodes('&', s);
-        return s;
+        formatted = StringUtils.replace(formatted, "%prefix%", Main.getInstance().getConfig().getString("Prefix"));
+        formatted = StringUtils.replace(formatted, "%player%", p.getName());
+        formatted = StringUtils.replace(formatted, "%onjoin-player-displayname%", p.getDisplayName());
+        formatted = StringUtils.replace(formatted, "%onjoin-player-uuid%", p.getUniqueId().toString());
+        formatted = StringUtils.replace(formatted, "%onjoin-player-gamemode%", p.getGameMode().name());
+        formatted = StringUtils.replace(formatted, "%onjoin-world%", p.getWorld().getName());
+        formatted = StringUtils.replace(formatted, "%onjoin-player-health%", String.valueOf(p.getHealth()));
+        formatted = StringUtils.replace(formatted, "%onjoin-player-max-health%", String.valueOf(p.getMaxHealth()));
+        formatted = StringUtils.replace(formatted, "%onjoin-max-players%", String.valueOf(Bukkit.getServer().getMaxPlayers()));
+        formatted = StringUtils.replace(formatted, "%onjoin-online-players%", String.valueOf(Bukkit.getServer().getOnlinePlayers().size()));
+        formatted = ChatColor.translateAlternateColorCodes('&', formatted);
+        return formatted;
     }
 }
