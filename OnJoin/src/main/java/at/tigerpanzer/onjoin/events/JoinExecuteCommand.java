85c41ce2fed248d1137d2e47fbf75bd81ca6b96b
Plajer <worthplay.pt@onet.pl>
2018-10-02 17:21:27 +0200

    Avoid to deep nesting

--- OnJoin/src/main/java/at/tigerpanzer/onjoin/events/JoinExecuteCommand.java
+++ OnJoin/src/main/java/at/tigerpanzer/onjoin/events/JoinExecuteCommand.java
@@ -11,7 +11,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import java.util.List;
 
 public class JoinExecuteCommand implements Listener {
-    
+
     private Main plugin;
 
     public JoinExecuteCommand(Main plugin) {
@@ -22,22 +22,24 @@ public class JoinExecuteCommand implements Listener {
     @EventHandler
     public void onJoin(final PlayerJoinEvent e) {
         Player p = e.getPlayer();
-        if((p.hasPermission("OnJoin.ExecuteCommand")) || (p.hasPermission("OnJoin.*"))) {
-            if(plugin.getConfig().getString("ExecuteCommand.CommandOn").contains("true")) {
-                final List<String> commands = plugin.getConfig().getStringList("ExecuteCommand.Commands");
-                for(String command : commands) {
-                    String[] parts = command.split(";");
-                    String sender = parts[0];
-                    String cmd = Utils.setPlaceholders(p, parts[1]);
-                    if(sender.equalsIgnoreCase("console")) {
-                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd);
-                    } else {
-                        if(!sender.equalsIgnoreCase("player")) {
-                            continue;
-                        }
-                        p.performCommand(cmd);
-                    }
+        if(!p.hasPermission("OnJoin.ExecuteCommand") || !p.hasPermission("OnJoin.*")) {
+            return;
+        }
+        if(!plugin.getConfig().getBoolean("ExecuteCommand.CommandOn")) {
+            return;
+        }
+        final List<String> commands = plugin.getConfig().getStringList("ExecuteCommand.Commands");
+        for(String command : commands) {
+            String[] parts = command.split(";");
+            String sender = parts[0];
+            String cmd = Utils.setPlaceholders(p, parts[1]);
+            if(sender.equalsIgnoreCase("console")) {
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd);
+            } else {
+                if(!sender.equalsIgnoreCase("player")) {
+                    continue;
                 }
+                p.performCommand(cmd);
             }
         }
     }
