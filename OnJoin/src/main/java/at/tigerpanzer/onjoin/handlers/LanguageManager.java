b3e7a0211d0a658d1d378be498c06af313007873
Tigerpanzer_02 <37453987+Tigerpanzer02@users.noreply.github.com>
2018-11-04 15:49:22 +0100

    2.0.0Pre

--- OnJoin/src/main/java/at/tigerpanzer/onjoin/handlers/LanguageManager.java
+++ OnJoin/src/main/java/at/tigerpanzer/onjoin/handlers/LanguageManager.java
@@ -23,99 +23,18 @@ import java.util.Properties;
 public class LanguageManager {
 
     private static Main plugin;
-    private static Locale pluginLocale;
-    private static Properties properties = new Properties();
 
     public static void init(Main pl) {
         plugin = pl;
         if(!new File(plugin.getDataFolder() + File.separator + "language.yml").exists()) {
             plugin.saveResource("language.yml", false);
         }
-        registerLocales();
-        setupLocale();
     }
-
-    private static void registerLocales() {
-        LocaleRegistry.registerLocale(new Locale("English", "English", "en_GB", "Tigerkatze and POEditor contributors", Arrays.asList("default", "english", "en")));
-        LocaleRegistry.registerLocale(new Locale("German", "Deutsch", "de_DE", "Tigerkatze and POEditor contributors", Arrays.asList("deutsch", "german", "de")));
-    }
-
-    private static void loadProperties() {
-        if(isDefaultLanguageUsed()) {
-            return;
-        }
-        LocaleService service = ServiceRegistry.getLocaleService(plugin);
-        if(service.isValidVersion()) {
-            LocaleService.DownloadStatus status = service.demandLocaleDownload(pluginLocale.getPrefix());
-            if(status == LocaleService.DownloadStatus.FAIL) {
-                pluginLocale = LocaleRegistry.getByName("English");
-                Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "[OnJoin] Locale service couldn't download latest locale for plugin! English locale will be used instead!");
-                return;
-            } else if(status == LocaleService.DownloadStatus.SUCCESS) {
-                Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "[OnJoin] Downloaded locale " + pluginLocale.getPrefix() + " properly!");
-            } else if(status == LocaleService.DownloadStatus.LATEST) {
-                Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "[OnJoin] Locale " + pluginLocale.getPrefix() + " is latest! Awesome!");
-            }
-        } else {
-            pluginLocale = LocaleRegistry.getByName("English");
-            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "[OnJoin] Your plugin version is too old to use latest locale! Please update plugin to access latest updates of locale!");
-            return;
-        }
-        try {
-            properties.load(new FileReader(new File(plugin.getDataFolder() + "/locales/" + pluginLocale.getPrefix() + ".properties")));
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void setupLocale() {
-        String localeName = plugin.getConfig().getString("locale", "default").toLowerCase();
-        for(Locale locale : LocaleRegistry.getRegisteredLocales()) {
-            for(String alias : locale.getAliases()) {
-                if(alias.equals(localeName)) {
-                    pluginLocale = locale;
-                    break;
-                }
-            }
-        }
-        if(pluginLocale == null) {
-            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "[OnJoin] Plugin locale is invalid! Using default one...");
-            pluginLocale = LocaleRegistry.getByName("English");
-        }
-        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "[OnJoin] Loaded locale " + pluginLocale.getName() + " (" + pluginLocale.getOriginalName() + " ID: " +
-                pluginLocale.getPrefix() + ") by " + pluginLocale.getAuthor());
-        loadProperties();
-    }
-
-    public static boolean isDefaultLanguageUsed() {
-        return pluginLocale.getName().equals("English");
-    }
-
     public static List<String> getLanguageList(String path) {
-        if(isDefaultLanguageUsed()) {
-            return ConfigUtils.getConfig(plugin, "language").getStringList(path);
-        } else {
-            return Arrays.asList(Utils.colorMessage(path).split(";"));
-        }
+        return ConfigUtils.getConfig(plugin, "language").getStringList(path);
     }
 
     public static String getLanguageMessage(String message) {
-        if(isDefaultLanguageUsed()) {
-            return ConfigUtils.getConfig(plugin, "language").getString(message, "ERR_MESSAGE_NOT_FOUND");
-        }
-        try {
-            return properties.getProperty(ChatColor.translateAlternateColorCodes('&', message));
-        } catch(NullPointerException ex) {
-            MessageUtils.errorOccurred();
-            Bukkit.getConsoleSender().sendMessage("Game message not found!");
-            Bukkit.getConsoleSender().sendMessage("Please regenerate your language.yml file! If error still occurs report it to the developer!");
-            Bukkit.getConsoleSender().sendMessage("Access string: " + message);
-            return "ERR_MESSAGE_NOT_FOUND";
-        }
-    }
-
-
-    public static Locale getPluginLocale() {
-        return pluginLocale;
+        return ConfigUtils.getConfig(plugin, "language").getString(message, "ERR_MESSAGE_NOT_FOUND");
     }
 }
